04ce0cf2fd82b8e57dd00bb3b9bc2dafa32d5095
Devdbrenno <brennosantos1010@gmail.com>
2024-03-25 19:27:16 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;//Biblioteca com os comandos padrões do C#. Obs.: No .Net 5 ela não é mais necessária.
+
+namespace HelloWorldCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");//Console.WriteLine(); É um comando de impressão
+            Console.ReadLine();//Console.ReadLine(); Lê a linha e "congela" a tela até que o usuário pressione uma tecla
+        }
+    }
+}
